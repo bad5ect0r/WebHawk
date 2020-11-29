e7a1c4b9acc61a17c175ab503ec5983a4a1cb87e
bad5ect0r <bad5ect0r@pm.me>
2020-11-29 15:03:37 +1100

    Created send_pushover function
    This will send a notification on Pushover. Added a single basic
    test to ensure that a simple notification works. Maybe in the
    future we should check to see that attachments are sent
    successfully.

--- diffresults/utils.py
+++ diffresults/utils.py
@@ -1,4 +1,7 @@
 from git import Repo
+from django.conf import settings
+
+import requests
 
 
 def create_gitdir(path):
@@ -6,3 +9,34 @@ def create_gitdir(path):
         path.mkdir(parents=True)
 
     return Repo.init(path)
+
+
+def send_pushover(title, message, attachment=None, **kwargs):
+    assert settings.PUSHOVER_API_TOKEN is not None, 'You need to set the pushover token.'
+    assert settings.PUSHOVER_API_USER is not None, 'You need to set the pushover user.'
+
+    token = settings.PUSHOVER_API_TOKEN
+    user = settings.PUSHOVER_API_USER
+    url = 'https://api.pushover.net/1/messages.json'
+    files = None
+
+    post_data = {
+        'token': token,
+        'user': user,
+        'message': message,
+        'title': title,
+    }
+
+    if attachment is not None:
+        assert type(attachment) is tuple, '''
+        attachment must be of type tuple(). It should looke like so:
+
+        ('image.EXT', file('file.EXT', 'rb'), 'MIME')
+        '''
+        files = {
+            'attachment': attachment
+        }
+
+    resp = requests.post(url, data=dict(post_data, **kwargs), files=files)
+
+    return resp
