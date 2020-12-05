f9085135b1a3b9cb1b2aff947de84acce8ea31d6
bad5ect0r <bad5ect0r@pm.me>
2020-12-05 17:03:57 +1100

    Fetch before save.
    Use the pre_save signal to save rather than the post_save signal.

--- diffresults/signals.py
+++ diffresults/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 
 from diffresults.models import Url
@@ -6,10 +6,10 @@ from diffresults.models import Url
 from django_q.tasks import Schedule
 
 
-@receiver(post_save, sender=Url)
-def fetch_saved_url(sender, instance, created, **kwargs):
-    if created:
-        instance.fetch()
+@receiver(pre_save, sender=Url)
+def fetch_saved_url(sender, instance, **kwargs):
+    if instance._state.adding:
+        instance.fetch(do_save=False)
 
 
 @receiver(post_save, sender=Url)
